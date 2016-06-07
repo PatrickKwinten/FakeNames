82e9fbfa0c37070e6d8a4500c2a7f6f18ed63995
Patrick Kwinten <patrick.kwinten@gmail.com>
2016-06-07 21:14:47 +0200

    Added select inputs to the datatable

--- ODP/Code/ScriptLibraries/datatables.js
+++ ODP/Code/ScriptLibraries/datatables.js
@@ -13,7 +13,7 @@ $(document).ready( function() {
 
 });
 
-function initPersons() {
+function initPersons2() {
 	var db = $("#persons").DataTable();
 	db.destroy();
 	localStorage.clear();
@@ -40,5 +40,55 @@ function initPersons() {
 		}				
 		]
 	});
+}
+
+function initPersons(){
+	var db = $("#persons").DataTable();
+	db.destroy();
+	localStorage.clear();
+	var table = $("#persons").DataTable( {
+		stateSave : saveState,
+		fixedHeader: true,
+		"language" : {
+			"lengthMenu" : "Entries per page _MENU_",
+			// "info" : "Page _PAGE_ of _PAGES_",
+			"infoEmpty" : "No entries found",
+			"infoFiltered" : ""
+		},
+		scrollY : yScroll,
+		"ajax" : "api.xsp/Persons" ,
+		"columns" : [ 
+		{
+			data : "firstname",
+			"defaultContent": "<i>Not set</i>"
+		},{
+			data : "lastname",
+			"defaultContent": "<i>Not set</i>"
+		},{
+			data : "company"
+		}				
+		],
+		initComplete: function(){
+			this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+		}
+	});
+	
 }
 
